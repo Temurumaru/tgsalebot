62fdb4471281d6912c34cdde5b7b5d827a8c13cd
Temurumaru <temurumaru@yandex.com>
2023-04-15 14:18:49 +0500

    deleted:    src/stages/create_admin.php 	modified:   src/stages/create_user.php 	deleted:    src/stages/delete_admin.php

--- src/stages/create_user.php
+++ src/stages/create_user.php
@@ -6,22 +6,22 @@ global $TG;
 global $message;
 
 
-if(count(R::find("users", "chat_id = ?", [(string)$TG -> data['chat']['id']])) == 0) {
+// if(count(R::find("users", "chat_id = ?", [(string)$TG -> data['chat']['id']])) == 0) {
 
-  $admin = R::dispense("users");
+//   $admin = R::dispense("users");
 
-  $admin -> name = $TG -> data['sender']['last_name']." ".$TG -> data['sender']['first_name'];
-  $admin -> chat_id = (string)$TG -> data['chat']['id'];
+//   $admin -> name = $TG -> data['sender']['last_name']." ".$TG -> data['sender']['first_name'];
+//   $admin -> chat_id = (string)$TG -> data['chat']['id'];
   
-  $admins = R::findAll("admin");
-  $rnd = array_rand($admins);
+//   $admins = R::findAll("admin");
+//   $rnd = array_rand($admins);
 
-  $admin -> admin_id = (string)$admins[$rnd] -> id;
+//   $admin -> admin_id = (string)$admins[$rnd] -> id;
 
-  R::store($admin);
+//   R::store($admin);
 
-  $TG -> answerText("Assalomu aleykum <b>".$TG -> data['sender']['last_name']." ".$TG -> data['sender']['first_name']."</b>!\nSiz admin Boldingiz!");
+//   $TG -> answerText("Assalomu aleykum <b>".$TG -> data['sender']['last_name']." ".$TG -> data['sender']['first_name']."</b>!\nSiz admin Boldingiz!");
 
-} else {
-  $TG -> answerText("Siz adminsiz token kerakmas!");
-}
+// } else {
+//   $TG -> answerText("Siz adminsiz token kerakmas!");
+// }
